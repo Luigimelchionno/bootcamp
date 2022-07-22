d99a5e3448d8c756b5f9485e07fb19d37cd8fe17
Luigimelchionno <luigi.melchionno@gmail.com>
2022-07-22 17:34:43 +0200

    add latest method

--- src/main/java/it/accenture/bootcamp/controllers/EditionController.java
+++ src/main/java/it/accenture/bootcamp/controllers/EditionController.java
@@ -1,15 +1,27 @@
 package it.accenture.bootcamp.controllers;
 
+import it.accenture.bootcamp.mapstruct.EditionMapper;
+import it.accenture.bootcamp.models.Edition;
+import it.accenture.bootcamp.services.implementations.CourseCrudService;
+import it.accenture.bootcamp.services.implementations.EditionCrudService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.accenture.bootcamp.dtos.EditionDTO;
 
+import java.util.Date;
+import java.util.List;
+
 @RestController
 @RequestMapping("edition")
 public class EditionController {
-
+    private EditionCrudService crudService;
+    public EditionController(EditionCrudService crudService){
+        this.crudService = crudService;
+    }
     @GetMapping
     public Iterable<EditionDTO> getAll() {
         // simulation of getting the edition from a database
@@ -25,5 +37,12 @@ public class EditionController {
         // return the DTO
         // return editions.stream().map(EditionDTO::fromEdition).toList();
         return null;
+    }
+    @GetMapping
+    public Iterable<EditionDTO> getLatest4Editions(@PathVariable Date date){
+        List<Edition> editions =  crudService.findLatestEdition(date);
+        var dtos = editions.stream().map(EditionMapper.INSTANCE::fromEdition).toList();
+        return ResponseEntity.ok(dtos);
+        return ;
     }
 }
